b7aad02658debba0c2c9880464b0b676060ac8f1
Aaditya <yaduupreti8@gmail.com>
2026-02-18 20:23:42 +0545

    day7 class

--- datascience/bin/activate.fish
+++ datascience/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/adu/Desktop/Day1/datascience"
+set -gx VIRTUAL_ENV "/Users/adu/Desktop/vrit-python/datascience"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
